59f073e259ed95e1ad0c828721666d01259b59d0
myusrilh <yusrilhasriansyah@gmail.com>
2021-03-21 11:28:00 +0700

    add pertemuan_6

---
+++ src/pertemuan_6/PictureInPicture.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PictureInPicture : MonoBehaviour
+{
+    public enum hAlignment{left, center, right};
+    public enum vAlignment{top, middle, bottom};
+    public hAlignment horAlign = hAlignment.left;
+    public vAlignment verAlign = vAlignment.top;
+    public enum UniteIn{pixels, screen_percentage};
+    public UniteIn unit = UniteIn.pixels;
+    public int width = 50;
+    public int height = 50;
+    public int xOffset = 0;
+    public int yOffset = 0;
+    public bool update = true;
+    public int hsize, vsize, hloc, vloc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AdjustCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(update) 
+        AdjustCamera();
+    }
+
+    void AdjustCamera(){
+        int sw = Screen.width;
+        int sh = Screen.height;
+        float swPercent = sw * 0.01f;
+        float shPercent = sh * 0.01f;
+        float xOffPercent = xOffset * swPercent;
+        float yOffPercent = yOffset * shPercent;
+        int xOff;
+        int yOff;
+        if(unit == UniteIn.screen_percentage){
+            hsize = width * (int)swPercent;
+            vsize = height * (int)shPercent;
+            xOff = (int)xOffPercent;
+            yOff = (int)yOffPercent;
+        }else{
+            hsize = width;
+            vsize = height;
+            xOff = xOffset;
+            yOff = yOffset;
+        }
+        switch(horAlign){
+            case hAlignment.left:
+                        hloc = xOff;
+                        break;
+            case hAlignment.right:
+                        int justifiedRight = (sw-hsize);
+                        hloc = (justifiedRight-xOff);
+                        break;
+            case hAlignment.center:
+                        float justifiedCenter = (sw * 0.5f) - (hsize * 0.5f);
+                        hloc = (int)(justifiedCenter - xOff);
+                        break;
+        }
+        switch (verAlign){
+            case vAlignment.top:
+                int justifiedTop = sh - vsize;
+                vloc = (justifiedTop - (yOff));
+                break;
+            case vAlignment.bottom:
+                vloc = yOff;
+                break;
+            case vAlignment.middle:
+                float justifiedMiddle = (sh * 0.5f) - (vsize * 0.5f);
+                vloc = (int)(justifiedMiddle - yOff);
+                break;
+        }
+        GetComponent<Camera>().pixelRect = new Rect(hloc,vloc,hsize,vsize);
+    }
+}
