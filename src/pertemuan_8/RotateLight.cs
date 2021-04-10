9693c6c815ec793e289209476498c4ba8d6f4bef
myusrilh <yusrilhasriansyah@gmail.com>
2021-04-10 23:05:55 +0700

    add pertemuan_8

---
+++ src/pertemuan_8/RotateLight.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+public class RotateLight : MonoBehaviour
+{
+    public float speed = -1.0f;
+    void Update()
+    {
+        transform.Rotate(Vector3.right * speed * Time.deltaTime);
+    }
+}
---
+++ src/pertemuan_8/UpdateProbe.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class UpdateProbe : MonoBehaviour
+{
+private ReflectionProbe probe;
+    void Awake()
+    {
+        probe = GetComponent<ReflectionProbe>();
+        probe.RenderProbe();
+    }
+    public void RefreshProbe()
+    {
+        probe.RenderProbe();
+    }
+}
