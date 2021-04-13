ef2bbe231eed1bcc8e343dabd224421a2805d07b
myusrilh <yusrilhasriansyah@gmail.com>
2021-04-13 14:22:57 +0700

    add pertemuan_9 (UTS)

---
+++ src/pertemuan_9/GetChest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetChest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text chestText;
+    private bool carryingChest = false;
+    void Start()
+    {
+        UpdateChestText();
+    }
+
+    void OnCollisionEnter(Collision hit){
+        if(hit.gameObject.tag == "Chest"){
+            carryingChest = true;
+            UpdateChestText();
+            Destroy(hit.gameObject);
+        }
+    }
+
+    void UpdateChestText()
+    {
+        string chestMessage = "Chest : belum didapat";
+        if (carryingChest) chestMessage = "Chest : sudah didapat. Selamat!";
+        chestText.text = chestMessage;   
+    }
+}
---
+++ src/pertemuan_9/MovePlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody _rigidBody;
+    [SerializeField] private float _movespeed = 2f;
+    [SerializeField] private float _turnspeed = 30f;
+
+    [SerializeField] private float _jumpForce = 200f;
+    private bool _shouldJump;
+    void Awake() => _rigidBody = GetComponent<Rigidbody>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Q)) transform.Rotate(Time.deltaTime * _turnspeed * Vector3.down);
+        
+        if(Input.GetKey(KeyCode.E)) transform.Rotate(Time.deltaTime * _turnspeed * Vector3.up);
+
+        if(Input.GetKey(KeyCode.D)) transform.position += Time.deltaTime * _movespeed * transform.right;
+
+        if(Input.GetKey(KeyCode.A)) transform.position += Time.deltaTime * _movespeed * -transform.right; //negative right give left values
+
+        if(Input.GetKey(KeyCode.W)) transform.position += Time.deltaTime * _movespeed * transform.forward;
+
+        if(Input.GetKey(KeyCode.S)) transform.position += Time.deltaTime * _movespeed * -transform.forward; //negative forward give backward values
+
+        if(_shouldJump == false){
+            _shouldJump = Input.GetKeyDown(KeyCode.Space);
+        }
+
+    }
+
+    void FixedUpdate(){
+        if(_shouldJump){
+            _rigidBody.AddForce(_jumpForce * Vector3.up);
+            _shouldJump = false;
+        }
+    }
+}
